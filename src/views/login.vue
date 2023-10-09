dcbf04861b972f31402db9abf74eee41fde90e1c
Saitama <604747948@qq.com>
2023-10-09 17:02:57 +0800

    精简版

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">盖伦后台管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -73,7 +73,7 @@ export default {
       codeUrl: "",
       loginForm: {
         username: "admin",
-        password: "123456",
+        password: "123456", 
         rememberMe: false,
         code: "",
         uuid: ""
